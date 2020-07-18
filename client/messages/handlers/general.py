8401efbbb36e78d9d3440f6e485c6df43d707676
its-tn10 <its.tn10@tamu.edu>
2020-07-18 08:33:12 +0200

    Mixed arguments for join()

--- client/messages/handlers/general.py
+++ client/messages/handlers/general.py
@@ -8,7 +8,7 @@ async def broadcast(client, message):
     print(f'NEW MESSAGE: {message}')
 
 async def list_topics(client, topics):
-    print(f'TOPICS LIST: {topics.join(",")}')
+    print(f'TOPICS LIST: {",".join(topics)}')
 
 async def unread(client, message):
     print(f'UNREAD MESSAGE: {message}')
